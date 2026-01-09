82a409dcc805c3293beea5b8cb002f6826f73c9f
“daydr3am” <2669217803@qq.com>
2026-01-09 17:05:28 +0800

    博客v1.0

---
+++ site.config.ts
@@ -0,0 +1,64 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+    url: 'https://youllour.github.io/',  //你网站的URL
+    favicon: "public\\icon.png",	// 网页图标链接
+    lang: 'zh-CN',  //默认语言
+    title: "DayDr3am's Blog",  //网站标题
+    subtitle: '在我们身上造成黑夜的，也可能留下一些星星给我们。',//网站副标题
+    author: {
+        name: 'DayDr3am',//博主名称
+        avatar: "C:\\Users\\daydream\\Pictures\\Camera Roll\\1.png",	//头像链接
+        status: {
+            emoji: '💛'	// 头像旁边的emoji
+        },
+    },
+
+    description: '欢迎来到我的博客！☆⌒(*＾-゜)v THX!!',  //简介
+    social: [
+        {
+            name: 'RSS',
+            link: '/atom.xml',  //这个是博客自带的RSS订阅，尽量留着，方便其他博友为你订阅
+            icon: 'i-ri-rss-line',
+            color: 'orange',
+        },
+        {
+            name: 'GitHub',
+            link: 'https://github.com/youllour',  //这里填写你的GitHub地址，不需要的话删除此字段即可
+            icon: 'i-ri-github-line',
+            color: '#6e5494',
+        },
+        {
+            name: '哔哩哔哩',
+            link: 'https://space.bilibili.com/361515508?spm_id_from=333.1007.0.0',  //这里填写你的BiliBili地址，不需要的话删除此字段即可
+            icon: 'i-ri-bilibili-line',
+            color: '#FF8EB3',
+        },
+        {
+            name: 'E-Mail',
+            link: 'mailto:2669217803@qq.com', //这里在mailto后面填写你的Email地址，不需要的话删除此字段即可
+            icon: 'i-ri-mail-line',
+            color: '#8E71C1',
+        },
+    ],
+
+    search: {
+        enable: true,
+    },
+    comment: {
+        enable: true
+    },
+    statistics: {
+        enable: true,
+        readTime: {
+            /**
+             * 阅读速度
+             */
+            speed: {
+                cn: 300,
+                en: 200,
+            },
+        },
+    },
+
+})
---
+++ valaxy.config.ts
@@ -0,0 +1,80 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+
+// add icons what you will need
+
+const safelist = [
+    'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+    // site config see site.config.ts
+
+    theme: 'yun',
+
+    themeConfig: {
+        banner: {
+            enable: true,
+            title: '白日梦',
+        },
+        pages: [
+            {
+                name: '网络世界的小伙伴们',
+                url: '/links/',
+                icon: 'i-ri-open-arm-line',  //这里的icon是Valaxy自带的图标，你可以在https://icones.js.org/找到你需要的图标，然后复制到icon字段中
+                //这里的ico我踩过坑，所以多说两句，这里的ICO复制名字即可，但是你需要在前面添加i-ri-【ICO名字】
+                color: 'hotpink',
+            },
+            {
+                name: '分类',
+                url: '/categories/',
+                icon: 'i-ri-apps-line',
+                color: 'dodgerblue',
+            },
+            {
+                name: '标签',
+                url: '/tags/',
+                icon: 'i-ri-bookmark-3-line',
+                color: 'dodgerblue',
+            },
+        ],
+        colors: {
+            primary: "#D69B54",
+        },
+        //页脚
+        footer: {
+            since: 2023,
+            powered: true, //这里是显示Valaxy驱动信息的，尊重作者劳动成果，我选择开启
+            beian: {
+                enable: false,
+                icp: '',  //这里是备案号，如果你不需要备案号，可以将上面的enable改为false即可
+            },
+        },
+
+        //背景图,这里为我自己添加的字段
+        bg_image: {
+            enable: true,  //这里是背景图的设置，你可以设置白日模式和夜间模式的背景图，如果你不需要背景图，可以将上面的enable改为false即可
+            url: "public\\1730014557261.jpg",	// 白日模式背景
+            dark: "public\\e1a0c1adcbbbe143903ed8e86912a03f256667467.jpg",	// 夜间模式背景
+        },
+
+        //鼠标点击烟花特效
+        fireworks: {
+            enable: true,
+            colors: ['#FFE57D', '#FFCD88', '#E6F4AD']
+        },
+
+    },
+    unocss: { safelist },
+    siteConfig: {
+        // 启用评论
+        comment: {
+            enable: false  //这里是评论的设置，如果你不需要评论，可以将enable改为false即可
+        },
+    },
+})
+
